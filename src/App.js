e086ead73ee4ff1d3a41fd73596ee32bcdcc920c
Josiah Cavitana <josiahcavitana@kenns-MacBook-Air.local>
2020-08-02 00:34:30 +0800

    dsss

--- src/App.js
+++ src/App.js
@@ -134,21 +134,5 @@ function App() {
   );
 }
 
-// export default EventLandingPage;
-// export default HotelTravelLandingPage;
-// export default AgencyLandingPage;
-// export default SaaSProductLandingPage;
-// export default RestaurantLandingPage;
-// export default ServiceLandingPage;
-// export default HostingCloudLandingPage;
-
-// export default LoginPage;
-// export default SignupPage;
-// export default PricingPage;
-// export default AboutUsPage;
-// export default ContactUsPage;
-// export default BlogIndexPage;
-// export default TermsOfServicePage;
-// export default PrivacyPolicyPage;
 
 export default App;
